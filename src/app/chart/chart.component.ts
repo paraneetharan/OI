4b4628424e9e7c013fa4fb34d2f450e3d225edda
Paraneetharan N <paraneetharan@zestiot.io>
2023-03-01 09:11:40 +0530

    bug

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -49,7 +49,7 @@ export class ChartComponent implements OnInit {
           this.data_format(r)
         })
       }
-      if( current_time > "09:00" && current_time < "09.10"){
+      if( current_time > "09:00" && current_time < "09:10"){
         localStorage.removeItem("chart")
         this.chart_data = {}
       }
