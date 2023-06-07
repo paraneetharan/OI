93d908d8183818bf4fdb34c1f85fe3871835f926
Paraneetharan <Paraneetharan.zestiot.io>
2023-06-08 00:07:17 +0530

    router bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'home', pathMatch: 'full'},
-  {path: 'home', component:ChartComponent}
+  {path: '', component:ChartComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
